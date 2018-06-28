5bec0d04ac4a9d0b8c8e6065fdb5b4f6036b29de
Adrian T <ad.trawka@gmail.com>
2018-06-28 20:37:53 +0200

    Update HullAdmin fields

--- fitting/admin.py
+++ fitting/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Hull, Fitting, Module
+from .models import Hull
 
 
 class HullAdmin(admin.ModelAdmin):
-    fields = ['name', 'faction', 'tier']
+    fields = ['name', 'hull_class', 'faction', 'tier', 'slots_low', 'slots_medium', 'slots_high',
+              'slots_rigs', 'launchers', 'turrets', 'drone_capacity', 'drone_bandwith',
+              'calibration', 'power', 'cpu', 'capacitor']
 
 
 admin.site.register(Hull, HullAdmin)
